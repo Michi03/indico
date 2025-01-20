07aec41c605b518c03f0d626e4c47b6eecfa998c
Indico VM <mic.michael@gmx.de>
2025-01-20 15:12:55 +0100

    added additional [before_notification_send](https://docs.getindico.io/en/stable/plugins/signals/#indico.core.signals.core.before_notification_send) signals

--- indico/modules/rb/notifications/blockings.py
+++ indico/modules/rb/notifications/blockings.py
@@ -5,6 +5,7 @@
 # modify it under the terms of the MIT License; see the
 # LICENSE file for more details.
 
+from indico.core import signals
 from indico.core.notifications import email_sender, make_email
 from indico.web.flask.templating import get_template_module
 
@@ -18,6 +19,7 @@ def notify_request(owner, blocking, blocked_rooms):
     with owner.force_user_locale():
         tpl = get_template_module('rb/emails/blockings/awaiting_confirmation_email_to_manager.txt',
                                   owner=owner, blocking=blocking, blocked_rooms=blocked_rooms)
+        signals.core.before_notification_send.send('notify-rb-blocking-owner', owner=owner, blocking=blocking, blocked_rooms=blocked_rooms, template=tpl)
         return make_email(owner.email, template=tpl)
 
 
@@ -31,4 +33,5 @@ def notify_request_response(blocked_room):
     with user.force_user_locale():
         tpl = get_template_module('rb/emails/blockings/state_email_to_user.txt',
                                   blocking=blocked_room.blocking, blocked_room=blocked_room)
+        signals.core.before_notification_send.send('notify-rb-blocking-user', user=user, blocking=blocked_room.blocking, blocked_room=blocked_room, template=tpl)
         return make_email(user.email, template=tpl)
